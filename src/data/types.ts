5030474c4eaa543c84907780fc255b3d41b54b79
Loïc Ledoyen <ledoyen.loic@gmail.com>
2026-09-25 16:01:43 +0200

    feat: define simulation types

---
+++ src/data/types.ts
@@ -0,0 +1,29 @@
+export type Job = "farmer" | "forester" | "fisher" | "builder";
+
+export interface Parameters {
+  population: number;
+  initialMoney: number;
+  moneyEnabled: boolean;
+  mobility: number;
+  priceSensitivity: number;
+  productivityVariance: number;
+}
+
+export interface Agent {
+  id: number;
+  x: number;
+  y: number;
+  job: Job;
+  productivity: number;
+  money: number;
+}
+
+export interface Metrics {
+  minute: number;
+  population: number;
+  medianWealth: number;
+  gini: number;
+  foodPrice: number;
+  wealthBins: number[];
+  productivityBins: number[];
+}
